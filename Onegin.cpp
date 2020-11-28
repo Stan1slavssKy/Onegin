43705b24f302a285460eb3aa5fa3accca60e35e6
Stanislav <70342945+StanislavKy@users.noreply.github.com>
2020-11-28 23:49:42 +0300

    Update Onegin.cpp

--- Onegin.cpp
+++ Onegin.cpp
@@ -1,17 +1,27 @@
-#include <stdio.h>
+#include <stdio.h>                 //FILE *fopen(const char *fname, const char *mode)
 #include <stdlib.h>                //Открытие файла осуществляется с помощью функции fopen(),
 #include <sys/stat.h>              //которая возвращает указатель на структуру типа FILE,
 #include <sys/types.h>             //который можно использовать для последующих операций с файлом.
 #include <unistd.h>                //int stat(const char *file_name, struct stat *buf);
 
+int read_file (char*);
+
 int main ()
 {
-    char file_name[20] = "";
+    char file_name[] = "";
+    int size_of_file = 0;
 
     printf ("Please enter the name of the file you want to read: ");
     scanf  ("%s", file_name);
     printf ("You want read \"%s\"\n", file_name);
 
+    size_of_file = read_file (file_name);
+
+
+}
+
+int read_file (char* file_name)
+{
     FILE *file = fopen (file_name, "r");
 
     struct stat information_buffer = {};
@@ -30,7 +40,7 @@ int main ()
 
     int size_of_file = information_buffer.st_size;
     printf ("Size of \"%s\" is: %d bytes.\n", file_name, size_of_file);
-    
+
     char *file_buffer = (char*) calloc (size_of_file + 1, sizeof(char));
 
     if (file_buffer == NULL)
@@ -43,5 +53,4 @@ int main ()
 
     free (file_buffer);
 
-    return 0;
 }
